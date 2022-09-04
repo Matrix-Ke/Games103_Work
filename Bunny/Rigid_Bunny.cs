213d9906c168f69533af1dd26398e909c297a72f
Matrix <2513084541@qq.com>
2022-09-04 19:25:52 +0800

    加上一个衰减系数: 避免反复碰撞。

--- Bunny/Rigid_Bunny.cs
+++ Bunny/Rigid_Bunny.cs
@@ -107,8 +107,8 @@ public class Rigid_Bunny : MonoBehaviour
     //a plane <P, N>
     void Collision_Impulse(Vector3 P, Vector3 N)
     {
-        Vector3 Rr_collision = new Vector3(0, 0, 0);
-        Vector3 V_Collision = new Vector3(0, 0, 0);
+        Vector3 Rr_Sum_collision = new Vector3(0, 0, 0);
+        Vector3 V_Sum_Collision = new Vector3(0, 0, 0);
 
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         Vector3[] vertices = mesh.vertices;
@@ -131,8 +131,8 @@ public class Rigid_Bunny : MonoBehaviour
                 //判断速度方向是向平面内运动还是向外运动。如果是内则需要计算碰撞。
                 if (Vector3.Dot(V_i, N) < 0)
                 {
-                    Rr_collision += R_ri;
-                    V_Collision += V_i;
+                    Rr_Sum_collision += R_ri;
+                    V_Sum_Collision += V_i;
                     counter++;
                 }
             }
@@ -141,12 +141,12 @@ public class Rigid_Bunny : MonoBehaviour
         //计算冲量大小更新速度，角速度
         if (counter > 0)
         {
-            Rr_collision /= counter;
-            V_Collision /= counter;
+            Rr_Sum_collision /= counter;
+            V_Sum_Collision /= counter;
 
             //虚拟碰撞点：将速度collisonVelocity分解成水平向和垂直向，然后进行碰撞衰减计算
-            Vector3 V_N = Vector3.Dot(V_Collision, N) * N;
-            Vector3 V_T = V_Collision - V_N;
+            Vector3 V_N = Vector3.Dot(V_Sum_Collision, N) * N;
+            Vector3 V_T = V_Sum_Collision - V_N;
             //由库仑定律： a = max(1 - μt(1 + μn)||Vni||/||Vti||)
             float a = Mathf.Max(1 - friction * (1 + restitution) * V_N.magnitude / V_T.magnitude, 0);
             Vector3 V_N_new = -V_N * restitution;
@@ -155,15 +155,17 @@ public class Rigid_Bunny : MonoBehaviour
 
             //compute the impulse J， 计算冲量J， 需要注意冲量和冲量矩的区别。
             Matrix4x4 I_inverse = I_ref.inverse;
-            Matrix4x4 R_ri_cross = Convert_Cross_Matrix(Rr_collision);
+            Matrix4x4 R_ri_cross = Convert_Cross_Matrix(Rr_Sum_collision);
             Matrix4x4 K = MatrixMinus(MatrixTimesFloat(Matrix4x4.identity, 1 / mass), R_ri_cross * I_inverse * R_ri_cross);
-            Vector3 J = K.inverse * (V_new - V_Collision);
+            Vector3 J = K.inverse * (V_new - V_Sum_Collision);
 
             //通过冲量来更新刚体的速度和角速度
             V_1 = V_1 + J / mass;
             //冲量矩 R_ri * J, 这里使用Rr_collision是因为需要对多个碰撞点去平均值计算。
-            W_1 = W_1 + I_inverse.MultiplyVector(Vector3.Cross(Rr_collision, J));
+            W_1 = W_1 + I_inverse.MultiplyVector(Vector3.Cross(Rr_Sum_collision, J));
 
+            //系数μN碰撞后衰减，防止物体抖动。
+            restitution *= 0.9f;
         }
     }
 
@@ -217,9 +219,9 @@ public class Rigid_Bunny : MonoBehaviour
             Vector3 x_0 = transform.position;
             Quaternion q_0 = transform.rotation;
             //Position_frame1 = Position_frame0 + DeltaTime * velocity
-            Vector3 x_1 = x_0 + dt * v;
+            Vector3 x_1 = x_0 + dt * V_1;
             //Update angular status ,将角速度的变化量转换成四元数相乘。
-            Quaternion q_w = new Quaternion(dt * w.x / 2, dt * w.y / 2, dt * w.z / 2, 0.0f);
+            Quaternion q_w = new Quaternion(dt * W_1.x / 2, dt * W_1.y / 2, dt * W_1.z / 2, 0.0f);
             Quaternion q_1 = QuatAdd(q_0, q_w * q_0);
             q_1 = Quaternion.Normalize(q_1);
             // Part IV: Assign to the object
